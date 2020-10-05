f3f95ed8f72213bd54995188b77678101592aa7e
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2020-10-05 13:50:22 +0300

    Решение задачи Merge Sorted Array

--- src/main/java/homework/Solution.java
+++ src/main/java/homework/Solution.java
@@ -1,5 +1,28 @@
-package homework;/**
-*@author Ilia Moskalenko
-*/
+package homework;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author Ilia Moskalenko
+ */
 public class Solution {
+    public static void main(String[] args) {
+        int nums1[] = {1,2,3,0,0,0};
+        int nums2[] = {2,5,6};
+
+        Solution.merge(nums1, (nums1.length-nums2.length), nums2,3);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
 }

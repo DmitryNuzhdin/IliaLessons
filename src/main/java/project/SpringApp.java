29d71f6a30ea78a155b76be6e964e2d93355b143
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2020-11-06 11:25:54 +0300

    Осуществил внедрение зависимостей через Spring

--- src/main/java/project/SpringApp.java
+++ src/main/java/project/SpringApp.java
@@ -1,17 +1,20 @@
 package project;
 
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import project.services.IOService;
 
 /**
  * @author Ilia Moskalenko
  */
 @Configuration
-@ComponentScan("project")
+@ComponentScan(basePackages = "project")
 public class SpringApp {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringApp.class);
+        context.getBean(IOService.class).start();
 
     }
 }

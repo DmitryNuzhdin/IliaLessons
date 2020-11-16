9c6cbf445033cf34e713ebb8add23e8395b061df
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2020-11-16 13:04:51 +0300

    create JDBC class

--- src/main/java/project/JDBCAccess.java
+++ src/main/java/project/JDBCAccess.java
@@ -4,7 +4,24 @@ package project;
  * @author Ilia Moskalenko
  */
 
+import java.sql.*;
 
 public class JDBCAccess {
+    public static void main(String[] args){
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:h2:/home/ilia/home/IliaLessons");
+            Statement statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }

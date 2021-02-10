5ec7aac7475561f47ffeb2abdc99336564b27bdb
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2021-02-10 14:27:01 +0300

    Add Task and User Services for REST AppController

--- src/main/java/project/controllers/AppController.java
+++ src/main/java/project/controllers/AppController.java
@@ -8,6 +8,8 @@ import project.exceptions.TaskNotFoundException;
 import project.exceptions.UserExistsException;
 import project.exceptions.UserNotFoundException;
 import project.models.*;
+import project.services.TaskService;
+import project.services.UserService;
 
 import java.util.List;
 
@@ -17,56 +19,57 @@ public class AppController {
     private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @Autowired
-    private Model model;
+    private UserService userService;
+    @Autowired
+    private TaskService taskService;
 
-    @PostMapping(value = "/users")
+    @PostMapping(value = "/user")
     User createUser(@RequestBody UserData user) throws UserExistsException {
-        logger.debug("Received user: {}",user);
-       return model.createUser(user);
+        logger.info("Received user: {}", user.toString());
+        return userService.createUser(user);
     }
 
     @PostMapping(value = "user/{userId}/task")
     Task createTask(@PathVariable(value = "userId") Long userId, @RequestBody TaskData task) throws UserNotFoundException {
-        return model.createTask(userId, task);
+        return taskService.createTask(userId, task);
     }
 
     @GetMapping(value = "/task/{id}")
-    Task getTaskById (@PathVariable(value = "id") Long id) throws TaskNotFoundException {
+    Task getTaskById(@PathVariable(value = "id") Long id) throws TaskNotFoundException {
         logger.debug("Received task: {}", id);
-        return model.getTaskById(id);
+        return taskService.getTaskById(id);
     }
 
     @GetMapping(value = "user/{id}")
     User getUserById(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
-        return model.getUserById(userId);
+        return userService.getUserById(userId);
     }
 
-    @PutMapping(value = "/task/{taskId}/update")
+    @PutMapping(value = "/task/{taskId}")
     Task updateTask(@PathVariable(value = "taskId") Long taskId,
                     @RequestBody TaskData taskData) throws TaskNotFoundException {
-        return model.updateTask(taskId, taskData);
+        return taskService.updateTask(taskId, taskData);
     }
 
     @DeleteMapping(value = "/task/{id}")
-    void deleteTask(@PathVariable(value = "id") Long taskId) throws TaskNotFoundException{
-        model.deleteTask(taskId);
+    void deleteTask(@PathVariable(value = "id") Long taskId) throws TaskNotFoundException {
+        taskService.deleteTask(taskId);
     }
 
     @DeleteMapping(value = "user/{id}")
-    void deleteUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException{
-        model.deleteUser(userId);
+    void deleteUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
+        userService.deleteUser(userId);
     }
 
-    @GetMapping(value = "tasks/{userId}")
-    List<Task> getAllTasksOfUser(@PathVariable(value = "userId") long userId) throws UserNotFoundException{
-        return model.getAllTasksOfUser(userId);
+    @GetMapping(value = "user/{userId}/tasks")
+    List<Task> getAllTasksOfUser(@PathVariable(value = "userId") Long userId) throws UserNotFoundException {
+        return taskService.getAllTasksOfUser(userId);
     }
 
-    @GetMapping(value = "tasks/active/{userId}")
-    List<Task> getAllActiveTaskOfUser(@PathVariable(value = "userId") long userId) throws UserNotFoundException{
-        return model.getAllActiveTaskOfUser(userId);
+    @GetMapping(value = "user/{userId}/tasks/active")
+    List<Task> getAllActiveTaskOfUser(@PathVariable(value = "userId") Long userId) throws UserNotFoundException {
+        return taskService.getAllActiveTaskOfUser(userId);
     }
-
 
 
 }
